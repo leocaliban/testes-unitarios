fc9866d27d017b5994c94b1c88337dc972ce7fec
Leocaliban <leocaliban@gmail.com>
2018-04-03 11:29:40 -0300

    Assertivas com AssertThat

--- src/test/java/com/leocaliban/unit_test/servicos/LocacaoServiceTeste.java
+++ src/test/java/com/leocaliban/unit_test/servicos/LocacaoServiceTeste.java
@@ -1,5 +1,12 @@
 package com.leocaliban.unit_test.servicos;
 
+import static com.leocaliban.unit_test.utils.DataUtils.isMesmaData;
+import static com.leocaliban.unit_test.utils.DataUtils.obterDataComDiferencaDias;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+
 import java.util.Date;
 
 import org.junit.Assert;
@@ -23,8 +30,25 @@ public class LocacaoServiceTeste {
 		Locacao locacao = service.alugarFilme(usuario, filme);
 		
 		//verificacao
-		Assert.assertTrue(locacao.getValor() == 5.0);
+		Assert.assertEquals(5.0, locacao.getValor(), 0.01);
 		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
 		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(),DataUtils.obterDataComDiferencaDias(1)));
+	}
+	
+	@Test
+	public void testeAssertThat() {
+		//cenario
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Rafael");
+		Filme filme = new Filme("Anabelle", 2, 5.0);
+		
+		//acao
+		Locacao locacao = service.alugarFilme(usuario, filme);
+		
+		//verificacao
+		assertThat(locacao.getValor(), is(equalTo(5.0)));
+		assertThat(locacao.getValor(), is(not(6.0)));
+		assertThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+		assertThat(isMesmaData(locacao.getDataRetorno(),obterDataComDiferencaDias(1)), is(true));
 	}
 }
